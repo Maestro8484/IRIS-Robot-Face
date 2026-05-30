4bd04ce13cc76ef03816ba3da4c4477dd9e81d4d
SuperMaster <supermaster@localhost>
2026-05-30 17:07:33 -0600

    S80: replace start_workbench.ps1 with double-clickable .bat

---
+++ start_workbench.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+set "WORKBENCH=%~dp0tools\workbench"
+
+start "IRIS Workbench Server" cmd /k "cd /d "%WORKBENCH%" && python -m http.server 8080"
+
+timeout /t 1 /nobreak >nul
+
+start "" "http://localhost:8080"
+
+echo IRIS Workbench running at http://localhost:8080
+echo Close the server window to stop.
